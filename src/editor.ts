4ec88d6e785410ac905bc40b692acc6db7a8e15a
Max Nelson <maxnelso@gmail.com>
2025-06-14 21:26:19 -0700

    Start of a level editor

---
+++ src/editor.ts
@@ -0,0 +1,42 @@
+import paper from "paper";
+
+let currentPath = null;
+
+function resetPath() {
+  currentPath = new paper.Path({
+    strokeColor: paper.Color.random(),
+    strokeWidth: 10,
+    strokeCap: "round",
+  });
+}
+
+function addPoint(x, y) {
+  currentPath.add(new paper.Point(x, y));
+}
+
+addEventListener("keydown", function (event) {
+  console.log(event.key);
+  switch (event.key) {
+    case "Escape":
+      paper.project.clear();
+      resetPath();
+      break;
+    case "Enter":
+      console.log(currentPath);
+      resetPath();
+      break;
+    default:
+      console.log("Unhandled key");
+  }
+});
+
+addEventListener("mousedown", function (event) {
+  console.log(event.button);
+  switch (event.button) {
+    case 0:
+      addPoint(event.clientX, event.clientY);
+      break;
+    default:
+      console.log("Unknown mousedown");
+  }
+});
